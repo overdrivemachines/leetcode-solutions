bcff483ffde248dc0508e21946ee5e04241eef6b
Dipen Chauhan <get.dipen@gmail.com>
2023-07-25 01:43:36 -0700

    Time: 249 ms (11.83%) | Memory: 16.5 MB (46.46%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,37 +1,25 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         result = 0
-        count = dict()
-        left, right = 0, 0
-        while right < len(s):
-            # print(left, "...", right, end=" ")
-            # add right to the count dictionary
+        # count the occurrences of each character
+        count = {}
+        left = 0
+        for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
-            # most repeating char count in the window
-            max_count = 0
-            for c in count.values():
-                max_count = max(max_count, c)
-
-            # print(
-            #     s[left : right + 1],
-            #     count,
-            #     "max_count:",
-            #     max_count,
-            #     "window size:",
-            #     right - left + 1,
-            #     end=" ",
-            # )
 
             # Check if window is valid
-            if (right - left + 1) - max_count <= k:
-                result = max(result, right - left + 1)
-                right += 1
-                # print("valid")
-
-            else:
-                # print("invalid")
+            # When Window size - Count of the most repeating char in the window > k,
+            # move the left pointer to the right
+            # In other words when the number of characters to be replaced in the window is greater than k,
+            # move the left pointer to the right
+            while (right - left + 1 - max(count.values())) > k:
                 count[s[left]] -= 1
-                count[s[right]] -= 1
                 left += 1
 
+            result = max(result, right - left + 1)
+            while (right - left + 1 - max(count.values())) > k:
+                count[s[left]] -= 1
+                left += 1
+
+            result = max(result, right - left + 1)
         return result
