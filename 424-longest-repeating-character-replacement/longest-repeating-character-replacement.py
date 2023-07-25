9db17a6515bf55f844026d6bb40c1c9c41548a09
Dipen Chauhan <get.dipen@gmail.com>
2023-07-25 01:47:19 -0700

    Time: 227 ms (13.12%) | Memory: 16.2 MB (97.46%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -4,15 +4,18 @@ class Solution:
         # count the occurrences of each character
         count = {}
         left = 0
+        maxf = 0
         for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
+            maxf = max(maxf, count[s[right]])
 
             # Check if window is valid
             # When Window size - Count of the most repeating char in the window > k,
             # move the left pointer to the right
             # In other words when the number of characters to be replaced in the window is greater than k,
             # move the left pointer to the right
-            while (right - left + 1 - max(count.values())) > k:
+            # while (right - left + 1 - max(count.values())) > k:
+            while (right - left + 1 - maxf) > k:
                 count[s[left]] -= 1
                 left += 1
 
