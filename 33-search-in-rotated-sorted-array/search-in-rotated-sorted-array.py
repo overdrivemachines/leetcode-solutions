e6955050d76dbd605462b215919f60c6a4179226
Dipen Chauhan <get.dipen@gmail.com>
2023-07-30 01:34:04 -0700

    Time: 49 ms (91.33%) | Memory: 16.6 MB (73.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,37 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        result = -1  # the index of target in nums
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
+
         while left <= right:
-            # if we are in the sorted part of nums
-            if nums[left] < nums[right]:
-                # regular binary search
-                mid = (left + right) // 2
-                if nums[left] == target:
-                    return left
-                elif nums[right] == target:
-                    return right
-                elif nums[mid] == target:
-                    return mid
-                elif nums[mid] < target:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            # are we in left sorted part?
+            if nums[left] <= nums[mid]:
+                if (target > nums[mid]) or (target < nums[left]):
+                    # search right portion
                     left = mid + 1
                 else:
+                    # search left portion
                     right = mid - 1
             else:
-                mid = (left + right) // 2
-                if nums[mid] == target:
-                    return mid
-                if nums[mid] >= nums[left]:
-                    if (target >= nums[left]) and (target <= nums[mid]):
-                        right = mid - 1
-                    else:
-                        left = mid + 1
+                # if we are in right sorted part
+                if (target < nums[mid]) or target > nums[right]:
+                    right = mid - 1
                 else:
-                    if (target >= nums[mid]) and (target <= nums[right]):
-                        left = mid + 1
-                    else:
-                        right = mid - 1
+                    left = mid + 1
 
-        return result
-
+        return -1
