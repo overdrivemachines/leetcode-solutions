7c7b6fea896f4428942abfd98fd6a0f6b7a3b493
Dipen Chauhan <get.dipen@gmail.com>
2023-08-03 20:31:56 -0700

    Time: 56 ms (95.72%) | Memory: 20.3 MB (87.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -5,7 +5,7 @@
 #         self.next = None
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
+    def hasCycle_slow(self, head: Optional[ListNode]) -> bool:
         result = False
         if head == None or head.next == None:
             return False
@@ -20,3 +20,13 @@ class Solution:
                 my_set.add(node)
             node = node.next
         return result
+    
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
