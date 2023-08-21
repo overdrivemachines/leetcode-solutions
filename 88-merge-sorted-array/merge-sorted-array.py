5fefe6e248ac266a04fc1820a801d77a49c8426b
Dipen Chauhan <get.dipen@gmail.com>
2023-08-21 15:41:11 -0700

    Time: 46 ms (69.30%) | Memory: 16.5 MB (16.16%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,5 +1,5 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge_old(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         if n == 0:
             return
 
@@ -50,3 +50,16 @@ class Solution:
         # print("final: ", nums1)
         # print(holding)
 
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
