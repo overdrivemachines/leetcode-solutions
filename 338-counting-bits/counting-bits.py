f3481d01b5d0507d297a2659589ed8c9085629e7
Dipen Chauhan <get.dipen@gmail.com>
2023-09-07 17:31:54 -0700

    Time: 72 ms (69.82%) | Memory: 23.2 MB (22.50%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,20 +1,7 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        count = 0
-        while n > 0:
-            if n % 2 == 1:
-                count += 1
-            n = n // 2
-        return count
-
     def countBits(self, n: int) -> List[int]:
-        li = list()
-        i = 0
-        count = 0
-
-        while i <= n:
-            # find the number of 1s in the binary representation of i
-            li.append(self.hammingWeight(i))
-            i += 1
-
-        return li
+        count = [0] * (n + 1)
+        count[0] = 0
+        for i in range(n + 1):
+            count[i] = count[i >> 1] + (i & 1)
+        return count
